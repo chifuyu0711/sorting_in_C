cacd768d875217c57444c6a1a05bd05ed67bb164
chifuyu0711 <123553545+chifuyu0711@users.noreply.github.com>
2024-03-24 09:23:49 +0000

    test

--- main.c
+++ main.c
@@ -1,22 +1,146 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct student{
-    char* name;
-    int age;
-    char* city;
-} student;
+typedef struct Students
+{
+    char *name;
+} students;
 
-char* read_line(int fd){
-    char c;
-    int n = read(fd, &c, 1); 
+char *read_line(int fd)
+{
+    char *full_line = calloc(2, sizeof(char));
+    int bytes_read;
+    char buff = 0;
+    for (int i = 0;; i++)
+    {
+        bytes_read = read(fd, &buff, 1);
+  
+        if (bytes_read == 0 || buff == '\n')
+        {
+            full_line[i] = '\0';
+            break;
+        }
+        full_line[i] = buff;
+        full_line = realloc(full_line, (i + 2) * sizeof(char));
+    
+    }
+    return full_line;
 }
 
-student* load_csv_data(char* file_name){
+students *load_to_struct(char *full_line)
+{
+    students *students1 = malloc(sizeof(students));
+    students1->name = strdup(full_line);
+    return students1;
+}
+
+students **load_txt_data(char *file_name, int *size)
+{
     int fd = open(file_name, O_RDONLY);
+    int count = 0;
+    char *full_line;
+    students **students_array = NULL;
+
+    // Пропускаем первую строку
+    full_line = read_line(fd);
+    free(full_line);
+
+    while (1)
+    {
+        full_line = read_line(fd);
+        if (full_line == NULL)
+        {
+            // Ошибка чтения строки
+            break;
+        }
+        if (strlen(full_line) == 0)
+        {
+            free(full_line);
+            break;
+        }
+        students *students1 = load_to_struct(full_line);
+        students_array = realloc(students_array, (count + 1) * sizeof(students *));
+        if (students_array == NULL)
+        {
+            free(students1->name);
+            free(students1);
+            free(full_line);
+            break;
+        }
+        students_array[count++] = students1;
+        free(full_line);
+    }
 
     close(fd);
+    *size = count;
+    return students_array;
+}
+
+void swap(students **arr, int i, int j)
+{
+    students *temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+void bubble_sort(students **arr, int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (strcmp(arr[j]->name, arr[j + 1]->name) > 0)
+            {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+}
+
+void write_to_file(char *file_name, students **arr, int size)
+{
+    int fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC);
+
+    // Записываем заголовок
+    char *header = "Name,Age,City\n";
+    write(fd, header, strlen(header));
+
+    // Записываем отсортированные имена студентов
+    for (int i = 0; i < size; i++)
+    {
+        write(fd, arr[i]->name, strlen(arr[i]->name));
+        write(fd, "\n", 1);
+    }
+
+    close(fd);
+}
+
+int main()
+{
+    char *file_name = "data.csv";
+    int array_size;
+    students **students_array = load_txt_data(file_name, &array_size);
+
+    if (students_array != NULL)
+    {
+        bubble_sort(students_array, array_size);
+
+        write_to_file(file_name, students_array, array_size);
+
+        for (int i = 0; i < array_size; i++)
+        {
+            free(students_array[i]->name);
+            free(students_array[i]);
+        }
+        free(students_array);
+    }
+    else
+    {
+        printf("Failed to load data from file.\n");
+    }
+
+    return 0;
 }
